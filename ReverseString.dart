6b45a0dbe510d0b36394fd5a6fccf583716146bc
Thegudboy <rujanbhetwal@gmail.com>
2024-11-18 23:34:44 +0545

    Reverse A String

---
+++ ReverseString.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+void main() {
+  print('Enter a string: ');
+  String input = stdin.readLineSync()!;
+
+  String reversed = input.split('').reversed.join('');
+  print('Reversed string: $reversed');
+}
